9655d59c1b03f48c54120e2376924d7a32f5a2a3
jewookwak <jewoo2963@naver.com>
2022-07-01 19:59:47 +0900

    making function up

--- 제우/백준_13901_로봇/백준/test3.cpp
+++ 제우/백준_13901_로봇/백준/test3.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 using namespace std;
 
+void up(); void down(); void left(); void right();
+
+int num_obstacle;
 int main()
 {
 
-	int row, col, num_obstacle;
+	int row, col;
 	cin >> row, cin >> col;
 	cin >> num_obstacle;
 
-	
 	int** obstacle_location;
 	obstacle_location = new int* [num_obstacle];
 	for (int i = 0; i < num_obstacle; i++)
@@ -38,9 +40,28 @@ int main()
 	for (int i = 0; i < row; i++) {
 		map[i] = new int[col];
 	}
+	// map initialize
+	for (int i = 0; i < row; i++)
+		for (int j = 0; j < col; j++)
+			map[i][j] = 0;
 
+	// add abostacle to map
+	for (int i = 0; i < num_obstacle; i++) {
+		map[obstacle_location[i][0]][obstacle_location[i][1]] = -1;
+	}
 	
-	
+	// add current x,y to map
+	map[cur_x][cur_y] = -1;
+
+
+	// checking the map
+	cout << endl;
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++)
+			cout << map[i][j] << "  ";
+		cout << endl;
+	}
+
 
 	// memory deallocation
 	delete[] obstacle_location;
@@ -49,6 +70,58 @@ int main()
 		delete[] map[i];
 	delete[] map;
 
+	// up,down,left,right
+	for (int i = 0; i < 4; i++) {
+		switch (move[i]) {
+
+		case 1: 
+			up(cur_x,cur_y, obstacle_location);
+			break;
+		case 2:
+			down(cur_x, cur_y, obstacle_location);
+			break;
+		case 3:
+			left(cur_x, cur_y, obstacle_location);
+			break;
+		case 4:
+			right(cur_x, cur_y, obstacle_location);
+			break;
+		}
+	}
+	
+
+
 	cout << "finish" << endl;
 	return 0;
+}
+
+void up(int x,int y, int** obs_location) {
+	int temp_location[1000][2];
+	cout << "up" << endl;
+	for (int i = 0; i < num_obstacle; i++) {
+		if (obs_location[i][1] == y && obs_location[i][0] < x) {
+			for (int j = 0; j < 2; j++) {
+				temp_location[i][j] = obs_location[i][j];
+
+			}
+			
+		}
+	}
+
+}
+
+void down(int x, int y, int** obs_location) {
+	cout << "down" << endl;
+}
+
+void left(int x, int y, int** obs_location) {
+	cout << "left" << endl;
+}
+void right(int x, int y, int** obs_location) {
+	cout << "right" << endl;
+}
+
+int max_order(int **A) {
+
+
 }
