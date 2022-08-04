9626a5fcba4404774be57cb7d958fce13c69e7d1
jewookwak <jewoo2963@naver.com>
2022-08-04 14:19:08 +0900

    fixing

--- 제우/Greedy_Algorithm/Greedy_Algorithm/Minimum_Spanning_Tree_1197.cpp
+++ 제우/Greedy_Algorithm/Greedy_Algorithm/Minimum_Spanning_Tree_1197.cpp
@@ -6,17 +6,41 @@ using namespace std;
 
 struct Joint {
     int a, b, c; // a : first point, b : second point, c : weight of joint between a and b.
-    int num;
+    int parent;
 };
 
 bool compareW(Joint w1, Joint w2) {
     return w1.c < w2.c;
 }
 
-int NaiveDisjointSet(vector<Joint> v) {
+int find(int u, vector<Joint>& j)
+{
+    if (u == j[u].parent)
+        return u;
+    else
+        return find(j[u].parent, j);
+}
+void merge(int u, int v, vector<Joint>& j) {
+    u = find(u, j);
+    v = find(v, j);
+    if (u == v) return;
+    j[v].parent = u;
+}
+
+int NaiveDisjointSet(int num,vector<Joint>& v) {
     int min_total_weight = 0;
+    for (int i = 1; i < num; i++)
+    {
+        if((v[i].parent == v[i+1].parent) & (v[i].b == v[i+1].a) )
+        { }
+        else
+        {
+            merge(i, i + 1, v);
+            min_total_weight += v[i].c;
+        }
+            
 
-
+    }
 
     return min_total_weight;
 }
@@ -25,9 +49,12 @@ int main()
     int v, e;
     cin >> v >> e;
     vector<Joint> joints(v);
-
-    for (int i = 0; i < v; i++)
+    for (int i = 0; i < e; i++) 
+    {
         cin >> joints[i].a >> joints[i].b >> joints[i].c;
+        joints[i].parent = joints[i].a;
+    }
+        
 
     /*
     check print
@@ -45,7 +72,7 @@ int main()
         cout << joints[i].a << ", " << joints[i].b << ", " << joints[i].c << endl;
     */
     
-    cout << NaiveDisjointSet(joints) << endl;
+    cout << NaiveDisjointSet(e,joints) << endl;
 
 	return 0;
 }
